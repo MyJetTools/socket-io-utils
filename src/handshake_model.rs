c044d00af323cd865c0743944529f5c524563799
amigin <amigin@gmail.com>
2024-12-19 20:11:31 +0200

    Fixes

--- src/handshake_model.rs
+++ src/handshake_model.rs
@@ -1,5 +1,7 @@
 use serde::*;
 
+use crate::SocketIoSettings;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SocketIoHandshakeOpenModel {
     pub sid: String,
@@ -10,4 +12,16 @@ pub struct SocketIoHandshakeOpenModel {
     pub ping_timeout: i32,
     #[serde(rename = "maxPayload")]
     pub max_payload: i32,
+}
+
+impl SocketIoHandshakeOpenModel {
+    pub fn from_settings(sid: String, socket_io_settings: &SocketIoSettings) -> Self {
+        Self {
+            sid,
+            upgrades: vec![String::from("websocket")],
+            ping_interval: socket_io_settings.ping_interval.as_secs() as i32,
+            ping_timeout: socket_io_settings.ping_timeout.as_secs() as i32,
+            max_payload: socket_io_settings.max_payload_size as i32,
+        }
+    }
 }
